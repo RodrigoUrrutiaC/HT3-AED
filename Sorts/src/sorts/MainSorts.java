ce7a4fa66817a4de53b5808677d7a8306b7b9543
Mac17369 <Mac17369@uvg.edu.gt>
2018-02-09 00:11:14 -0600

    Gnome, Radix y merge sort creados

--- Sorts/src/sorts/MainSorts.java
+++ Sorts/src/sorts/MainSorts.java
@@ -18,22 +18,24 @@ public class MainSorts {
         Aleatorio a = new Aleatorio();
         Sorts s = new Sorts();
         
-//        int[] cadenaGnome = sort.GnomeSort(data);
-//        System.out.println("Gnome Sort:");
-//        System.out.println(Arrays.toString(cadenaGnome));
-//        
-//        int[] cadenaMerge = sort.MergeSort(data.lista);
-//        System.out.println("Merge Sort:");
-//        System.out.println(Arrays.toString(cadenaMerge));
-       
+        int[] cadenaGnome = s.gnomeSort(a.cadena);
+        System.out.println("Gnome Sort:");
+        System.out.println(Arrays.toString(cadenaGnome));
+
         int[] cadenaQuick = s.quickSort(a.cadena,0,2999);
         System.out.println("Quick Sort:");
         System.out.println(Arrays.toString(cadenaQuick));
       
-//        int[] cadenaRadix = sort.RadixSort(data);
-//        System.out.println("Radix Sort");
-//        System.out.println(Arrays.toString(cadenaRadix));
-       
+        int[] cadenaRadix = s.radixSort(a.cadena);
+        System.out.println("Radix Sort");
+        System.out.println(Arrays.toString(cadenaRadix));
+        
+        //Ya funciona pero imprime el orden un numero por fila
+        int[] inputArray =  {1, 3, 5, 2, 4, 6};
+        int[] sortedArray = s.divideAndConquer(a.cadena);
+        for (int i=0; i<sortedArray.length; i++) {
+        System.out.println(sortedArray[i]);
+     }
     }
-    
-}
+ }
+
--- Sorts/src/sorts/Sorts.java
+++ Sorts/src/sorts/Sorts.java
@@ -44,5 +44,116 @@ public class Sorts {
         
         return cadena; 
     }
+
+    //    ALGORITMO GNOME SORT OBTENIDO DE:
+    //    http://professorjava.weebly.com/gnome-sort.html
+    
+    public static int[] gnomeSort(int[] nums){ //takes unsorted array, returns sorted
+    int index=1; //start of search
+    int temp;
+    while(index<nums.length){ //until the array is fully sorted
+      if(nums[index]<nums[index-1]){ //compares nums[index] with nums[index-1]. if smaller, switch.
+        temp=nums[index];
+        nums[index]=nums[index-1];
+        nums[index-1]=temp;
+        index--; //must decrease index to recheck. since they have been swapped, the array may still be out of order
+        if(index==0){ //prevents index from going lower than 1
+          index=1;
+        }
+      }
+      else{
+        index++; //if sorted, go up
+      }
+    }
+    return(nums); //reaching the end of the array- completely sorted, returns nums
+  }
+        
+    //    ALGORITMO QUICK MERGE OBTENIDO DE:
+    //    https://github.com/andreivisan/Algorithms/blob/master/merge_sort/MergeSort.java
+    
+    
+    public int[] divideAndConquer(int[] inputArray) {
+    int n = inputArray.length;
+    if(n == 1) {
+    return inputArray;
+    }
+    int mid = n/2;
+    int[] leftArray = new int[mid];
+    int[] rightArray = new int[n - mid];
+    System.arraycopy(inputArray, 0, leftArray, 0, leftArray.length);
+    System.arraycopy(inputArray, leftArray.length, rightArray, 0, rightArray.length);
+    divideAndConquer(leftArray);
+    divideAndConquer(rightArray);
+    merge(leftArray, rightArray, inputArray);
+    return inputArray;
+    }
+
+    public void merge(int[] leftArray,
+    int[] rightArray,
+    int[] sortedArray) {
+      int leftArrayLength = leftArray.length;
+      int rightArrayLength = rightArray.length;
+      int i = 0;
+      int j = 0;
+      int k = 0;
+      while(i < leftArrayLength && j < rightArrayLength) {
+        if(leftArray[i] < rightArray[j]) {
+          sortedArray[k] = leftArray[i];
+          i++;
+        } else {
+          sortedArray[k] = rightArray[j];
+          j++;
+        }
+        k++;
+      }
+      while(i < leftArrayLength) {
+        sortedArray[k] = leftArray[i];
+        i++;
+        k++;
+      }
+      while(j < rightArrayLength) {
+        sortedArray[k] = rightArray[j];
+        j++;
+        k++;
+      }
+    }
+
+    //
+    //
+    
+    public static int[] radixSort(int[] old) {
+    // Loop for every bit in the integers
+    for (int shift = Integer.SIZE - 1; shift > -1; shift--) {
+        // The array to put the partially sorted array into
+        int[] tmp = new int[old.length];
+        // The number of 0s
+        int j = 0;
+ 
+        // Move the 0s to the new array, and the 1s to the old one
+        for (int i = 0; i < old.length; i++) {
+            // If there is a 1 in the bit we are testing, the number will be negative
+            boolean move = old[i] << shift >= 0;
+ 
+            // If this is the last bit, negative numbers are actually lower
+            if (shift == 0 ? !move : move) {
+                tmp[j] = old[i];
+                j++;
+            } else {
+                // It's a 1, so stick it in the old array for now
+                old[i - j] = old[i];
+            }
+        }
+ 
+        // Copy over the 1s from the old array
+        for (int i = j; i < tmp.length; i++) {
+            tmp[i] = old[i - j];
+        }
+ 
+        // And now the tmp array gets switched for another round of sorting
+        old = tmp;
+    }
+ 
+    return old;
+}
     
 }
